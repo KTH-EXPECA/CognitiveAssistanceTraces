5ed3e07285ae86844d08d17a451976255c3621aa
Manuel Osvaldo Olguín <molguin@kth.se>
2018-04-03 10:14:19 +0200

    remove redundant files

--- timing_information_legovideo.py
+++
@@ -1,9 +0,0 @@
-from parse_video_stream import read_video_stream
-from typing import Dict
-from scapy.all import *
-
-def find_videoframe_timestamps(pcap: str) -> Dict:
-    with rdpcap(pcap) as trace:
-        for pkt in trace:
-
-
